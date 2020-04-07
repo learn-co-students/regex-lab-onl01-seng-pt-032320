d5ae10bd42bdc93c56d33f1d6c595347db16bf53
Beverly Appleton <bfcain19@gmail.com>
2020-04-07 15:13:45 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,9 +11,9 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.match(/^[A-Z].+{.?!]$/}) ? true : false
+text.match(/^[A-Z].+[.?!]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-phone.match(/\(?\d{3})(?\d{3})(?\d{4}$/)) ? true : false
+phone.match(/\(?[0-9]{3}\)? ?[0-9]{3}( |\-)?[0-9]{4}/) ? true : false
 end
