034b2ed340204dd91b58f175e6ef5ff700835bde
Dina Kendle <dina.kendle@gmail.com>
2020-04-13 18:38:51 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,7 +1,7 @@
 require "pry"
 
 def starts_with_a_vowel?(word)
-  if word.match(/^[AEIOU]^[aeiou]/)
+  if word.match(/^[AEIOU]^[aeiou]$/)
     true 
   else 
     false
@@ -9,17 +9,21 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-  
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    if text.match(/^[A-Z]/) && text.match(/[\.]$/) 
+      true 
+    else 
+      false
+    end
 end
 
 def valid_phone_number?(phone)
-
+  
 end
